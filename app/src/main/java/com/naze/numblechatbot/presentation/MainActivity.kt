bae2607b46c893b35e1c8eea999d6cd67d2f729f
NaZe0320 <rwt1226@naver.com>
2023-04-01 05:34:32 +0900

    [init] hilt, room 추가

--- app/src/main/java/com/naze/numblechatbot/presentation/MainActivity.kt
+++ app/src/main/java/com/naze/numblechatbot/presentation/MainActivity.kt
@@ -3,7 +3,9 @@ package com.naze.numblechatbot.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.naze.numblechatbot.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
