9afc2cd4e4e7cc8101dc354150bc2947d89aea4f
NaZe0320 <rwt1226@naver.com>
2023-04-03 04:19:00 +0900

    [feat] chat, setting fragment 생성

---
+++ app/src/main/java/com/naze/numblechatbot/presentation/ChatFragment.kt
@@ -0,0 +1,14 @@
+package com.naze.numblechatbot.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.naze.numblechatbot.R
+import com.naze.numblechatbot.databinding.FragmentChatBinding
+import com.naze.numblechatbot.util.binding.BindingFragment
+
+class ChatFragment : BindingFragment<FragmentChatBinding>(R.layout.fragment_chat) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/naze/numblechatbot/presentation/SettingFragment.kt
@@ -0,0 +1,14 @@
+package com.naze.numblechatbot.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.naze.numblechatbot.R
+import com.naze.numblechatbot.databinding.FragmentSettingBinding
+import com.naze.numblechatbot.util.binding.BindingFragment
+
+class SettingFragment : BindingFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
