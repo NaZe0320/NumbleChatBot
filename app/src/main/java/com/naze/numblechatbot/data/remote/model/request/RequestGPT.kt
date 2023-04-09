7be5a22456038336e317176ab5076451c36789cd
NaZe0320 <rwt1226@naver.com>
2023-04-10 01:19:04 +0900

    [feat] RequestGPT temperature, 및 frequency Penalty 추가

--- app/src/main/java/com/naze/numblechatbot/data/remote/model/request/RequestGPT.kt
+++ app/src/main/java/com/naze/numblechatbot/data/remote/model/request/RequestGPT.kt
@@ -3,10 +3,9 @@ package com.naze.numblechatbot.data.remote.model.request
 import com.google.gson.annotations.SerializedName
 
 data class RequestGPT(
-    @SerializedName("prompt")
-    val prompt: String,
-    @SerializedName("max_tokens")
-    val maxTokens: Int? = null,
-    @SerializedName("model")
-    val model: String? = "text-davinci-003"
+    @SerializedName("prompt") val prompt: String,
+    @SerializedName("max_tokens") val maxTokens: Int? = null,
+    @SerializedName("model") val model: String? = "text-davinci-003",
+    @SerializedName("temperature") val temperature: Double?,
+    @SerializedName("Frequency Penalty") val frequencyPenalty: Double?
 )
